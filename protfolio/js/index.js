0f9bc006020d5452b368c420ce7cfaf29b6a32a8
dimpal-29 <dimpalnarkhede29@gmail.com>
2026-01-29 12:36:02 +0530

    Add files via upload

---
+++ protfolio/js/index.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+const navLinks = document.querySelectorAll('.nav-link');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
